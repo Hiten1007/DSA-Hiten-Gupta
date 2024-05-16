5cb1eeb663759c61213512cdb9463e34ade2ae12
Hiten1007 <159756777+Hiten1007@users.noreply.github.com>
2024-05-17 03:05:27 +0530

    Add files via upload

---
+++ Conditionalsandloops/BasicJavaPrograms/AreaCircle.java
@@ -0,0 +1,12 @@
+package Conditionalsandloops.BasicJavaPrograms;
+
+import java.util.*;
+
+public class AreaCircle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the radius of circle here : ");
+        int r = sc.nextInt();
+        System.out.print("The area of circle is : " + 3.14 * r * r);
+    }
+}

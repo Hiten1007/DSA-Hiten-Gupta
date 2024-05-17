eb70c9a50d272422fd13cfd6a3b4040c8f6841c1
Hiten1007 <159756777+Hiten1007@users.noreply.github.com>
2024-05-17 20:26:20 +0530

    Q26
    Addition Of Two Numbers

---
+++ Conditionalsandloops/BasicJavaPrograms/Addition.java
@@ -0,0 +1,15 @@
+package Conditionalsandloops.BasicJavaPrograms;
+
+import java.util.*;
+
+public class Addition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number here : ");
+        int a = sc.nextInt();
+        System.out.print("Enter a number here : ");
+        int b = sc.nextInt();
+
+        System.out.print("Their sum is : " + (a + b));
+    }
+}

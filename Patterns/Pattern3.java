72f64ded88f3f63849f26b53b7c7a6b01837ca60
Hiten1007 <159756777+Hiten1007@users.noreply.github.com>
2024-06-02 12:35:54 +0530

    3
    Pattern3

---
+++ Patterns/Pattern3.java
@@ -0,0 +1,15 @@
+package Patterns;
+
+public class Pattern3 {
+    public static void main(String[] args) {
+        pattern3(5);
+    }
+    static void pattern3(int n){
+        for(int i = 1; i <= n; i++){
+            for(int j = n; j>= i; j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

ebf275c94e83c5f4d425d3169ddcafbd7dbeb3f7
Hiten1007 <159756777+Hiten1007@users.noreply.github.com>
2024-07-01 01:12:33 +0530

    Add files via upload

---
+++ Linkedlist/LinkedListQuestions/RemoveDuplicatesfromSortedList.java
@@ -0,0 +1,20 @@
+package Linkedlist.LinkedListQuestions;
+
+
+
+public class RemoveDuplicatesfromSortedList {
+//    public void deleteDuplicates(){
+//        Node node = head;
+//        while(node.next != null){
+//            if(node.value == node.next.value){
+//                node.next = node.next.next;
+//                size--;
+//            }
+//            else{
+//                node = node.next;
+//            }
+//        }
+//        tail = node;
+//        tail.next = null;
+//    }
+}
